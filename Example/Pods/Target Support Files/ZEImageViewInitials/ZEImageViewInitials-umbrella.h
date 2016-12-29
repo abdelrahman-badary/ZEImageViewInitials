4eae557d92b26f64f95cbe54c57f6d16f4c4a19e
abdelrahman-badary <abdelrahman.badary@hotmail.com>
2016-12-29 05:46:31 +0200

    Adding updated project files

--- Example/Pods/Target Support Files/ZEImageViewInitials/ZEImageViewInitials-umbrella.h
+++ Example/Pods/Target Support Files/ZEImageViewInitials/ZEImageViewInitials-umbrella.h
@@ -2,8 +2,10 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "UIColor+ZEColors.h"
 #import "UIView+constraints.h"
 #import "ZEInitialsView.h"
+#import "ZERandomColorGenerator.h"
 
 FOUNDATION_EXPORT double ZEImageViewInitialsVersionNumber;
 FOUNDATION_EXPORT const unsigned char ZEImageViewInitialsVersionString[];
