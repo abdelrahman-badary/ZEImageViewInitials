032e6809400f3c41a4002933bfc4b6b118b5eb6c
abdelrahman-badary <abdelrahman.badary@hotmail.com>
2016-12-29 05:38:30 +0200

    Added category for custom UIColors

---
+++ ZEImageViewInitials/Classes/UIColor+ZEColors.h
@@ -0,0 +1,20 @@
+//
+//  UIColor+ZEColors.h
+//  Pods
+//
+//  Created by Abdelrahman Badary on 12/29/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ZEColors)
++(UIColor *)ZEPinkColor;
++(UIColor *)ZELightOrangeColor;
++(UIColor *)ZESkyBlueColor;
++(UIColor *)ZELightBrownColor;
++(UIColor *)ZEDarkGreenColor;
++(UIColor *)ZERedColor;
++(UIColor *)ZEGreenColor;
++(UIColor *)ZEDarkBlueColor;
+@end
---
+++ ZEImageViewInitials/Classes/UIColor+ZEColors.m
@@ -0,0 +1,44 @@
+//
+//  UIColor+ZEColors.m
+//  Pods
+//
+//  Created by Abdelrahman Badary on 12/29/16.
+//
+//
+
+#import "UIColor+ZEColors.h"
+
+@implementation UIColor (ZEColors)
++(UIColor*)ZEPinkColor
+{
+    return  [UIColor colorWithRed:234.0f/255.0f green:144.0f/255.0f blue:203.0f/255.0f alpha:1.0f];
+}
++(UIColor *)ZELightOrangeColor
+{
+    return  [UIColor colorWithRed:241.0f/255.0f green:210.0f/255.0f blue:206.0f/255.0f alpha:1.0];
+}
++(UIColor *)ZESkyBlueColor
+{
+    return [UIColor colorWithRed:151.0f/255.0f green:224.0f/255.0f blue:248.0f/255.0f alpha:1.0f];
+}
++(UIColor *)ZELightBrownColor
+{
+    return [UIColor colorWithRed:225.0f/255.0f green:128.0f/255.0f blue:125.0f/255.0f alpha:1.0f];
+}
++(UIColor *)ZEDarkGreenColor
+{
+    return  [UIColor colorWithRed:4.0f/255.0f green:81.0f/255.0f blue:123.0f/255.0f alpha:1.0f];
+}
++(UIColor *)ZERedColor
+{
+    return  [UIColor colorWithRed:196.0f/255.0f green:53.0f/255.0f blue:91.0f/255.0f alpha:1.0f];
+}
++(UIColor *)ZEGreenColor
+{
+    return  [UIColor colorWithRed:0.0f/255.0f green:173.0f/255.0f blue:27.0f/255.0f alpha:1.0f];
+}
++(UIColor *)ZEDarkBlueColor
+{
+    return [UIColor colorWithRed:18.0f/255.0f green:36.0f/255.0f blue:193.0f/255.0f alpha:1.0f];
+}
+@end
